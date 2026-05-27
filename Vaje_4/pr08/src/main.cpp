15488fd4dc3799d0185336f637dcd940a30020d6
Tjankulov <51080695+Tjankulov@users.noreply.github.com>
2026-05-27 22:11:07 +0200

    Update main.cpp

--- Vaje_4/pr08/src/main.cpp
+++ Vaje_4/pr08/src/main.cpp
@@ -2,8 +2,8 @@
 #include "WiFi.h"
 #include "WebSocketsClient.h"
 
-const char* ssid = "A1-39A3F1-EXT";
-const char* password = "nazaxa0060";
+const char* ssid = "...";
+const char* password = "...";
 
 int portNumber = 8811; // preko vrat 8811 se bo esp32 povezal na node.js strežnik
 
